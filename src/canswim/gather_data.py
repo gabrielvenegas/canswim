b38ba93fa2fdb3ecb785ae7aac4ee48d60fd6970
Gabriel Enrique <ged.venegas@gmail.com>
2025-06-06 18:55:17 -0300

    wip

--- src/canswim/gather_data.py
+++ src/canswim/gather_data.py
@@ -104,15 +104,15 @@ class MarketDataGatherer:
         stock_files = [
             # "test_stocks.csv"
             "IBD50.csv",
-            # "IBD250.csv",
-            # "ibdlive_picks.csv",
-            # "russell2000_iwm_holdings.csv",
-            # "sp500_ivv_holdings.csv",
-            # "nasdaq100_cndx_holdings.csv",
+            "IBD250.csv",
+            "ibdlive_picks.csv",
+            "russell2000_iwm_holdings.csv",
+            "sp500_ivv_holdings.csv",
+            "nasdaq100_cndx_holdings.csv",
             # "watchlist.csv",
             # "vti_total_market_stocks.csv",
-            # "ITB_holdings.csv",
-            # "IYM_holdings.csv",
+            "ITB_holdings.csv",
+            "IYM_holdings.csv",
             self.all_stocks_file,
         ]
         logger.info(
