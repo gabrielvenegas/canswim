8a1058f776c2de04074309c1e59d6d859b7c0a2f
Gabriel Enrique <ged.venegas@gmail.com>
2025-06-06 16:42:45 -0300

    wip

--- src/canswim/model.py
+++ src/canswim/model.py
@@ -721,9 +721,10 @@ class CanswimModel:
             min_samples=self.min_samples,
             start_date=start_date,
         )
-        self.covariates.load_data(
-            stock_tickers=self.stock_tickers, start_date=start_date
-        )
+        # self.covariates.load_data(
+        #     stock_tickers=self.stock_tickers, start_date=start_date
+        # )
+
         logger.info("Aligning tickers across all data sources...")
 
         # Get tickers that actually loaded into the targets object
