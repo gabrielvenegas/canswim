105338df94d89573ebf4db65040a6b8e57de803b
Gabriel Enrique <ged.venegas@gmail.com>
2025-06-18 16:07:57 -0300

    Comment out ticker list and use fixed stock group for forecasting

--- src/canswim/forecast.py
+++ src/canswim/forecast.py
@@ -68,7 +68,8 @@ class CanswimForecaster:
         target_sliced_list = []
         past_cov_list = []
         future_cov_list = []
-        tickers_list = self.canswim_model.targets_ticker_list
+        # tickers_list = self.canswim_model.targets_ticker_list
+        ticker_list = [] #
         # trim end of targets to specified forecast start date
         if forecast_start_date is not None:
             logger.debug(
@@ -261,7 +262,7 @@ class CanswimForecaster:
         # group tickers in workable sample sizes for each forecast pass
         # credit ref: https://stackoverflow.com/questions/434287/how-to-iterate-over-a-list-in-chunks
         for pos in range(0, len(stock_list), self.n_stocks):
-            stock_group = stock_list[pos : pos + self.n_stocks]
+            stock_group = ['CRDO'] # stock_list[pos : pos + self.n_stocks]
             self.canswim_model.load_data(
                 stock_tickers=stock_group, start_date=self.start_date
             )
