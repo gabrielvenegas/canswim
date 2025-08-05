31cea2a1a64289ce6929353f0e500f0023c72432
Bruno Branco <brunocbranco16@gmail.com>
2025-08-05 12:51:39 -0300

    fix start script

--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-echo "Starting"
-echo "Installing python libs"
+echo -e "\033[32mStarting\033[0m"
+echo -e "\033[32mInstalling python libs\033[0m"
 pip install -e .
-echo "Libs installed, copying env into .env file"
+echo -e "\033[32mLibs installed, copying env into .env file\033[0m"
 cp example.env .env
-echo ".env file created, installing vim"
+echo -e "\033[32m.env file created, installing vim\033[0m"
 apt update
 apt install -y vim
-echo "Installing nano for Gabriel cause he's afraid of Vim's superiority"
+echo -e "\033[32mInstalling nano cause Gabriel is afraid of vim!!!!\033[0m"
 apt install nano
-echo "Nano installed, installing tmux"
-echo apt install tmux
+echo -e "\033[32mNano installed, installing tmux\033[0m"
+apt install -y tmux
+echo -e "\033[32mTmux installed!\033[0m"
